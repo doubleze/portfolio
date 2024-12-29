68c5c7af1105bf03c61a9e27b5b56ee1609457a7
Orbit <doubleze1982@gamil.com>
2024-12-29 11:18:17 +0300

    update mate CSP9

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     assetsDir: 'assets'
+//   },
+//     server: {
+//       port: 3000,
+//       open: true,
+//   }
+// })
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: true,
+    hmr: {
+      protocol: "ws",
+    },
   },
-    server: {
-      port: 3000,
-      open: true,
-  }
-})
+});
