c8b683f4317d069163788909f305d637e22eb4ae
Orbit <doubleze1982@gamil.com>
2024-12-29 06:27:25 +0300

    Inicial commite portfolio

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import NavBR from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <>
+     <NavBR/>
+     <Hero />
+     <About />
+     <Contact />
+     <Footer />
+    </>
+  )
+}
+
+export default App
