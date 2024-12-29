5339b26dc018d869b64617e14ca95b3eca15f46d
Orbit <doubleze1982@gamil.com>
2024-12-29 08:05:27 +0300

    update mate CSP 7

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // This matches your repository name
+  base: '/portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
